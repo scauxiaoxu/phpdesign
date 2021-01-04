3c72628bbabbaa8fa825965991e089705b6a280e
xiaoxu <657978106@qq.com>
2021-01-04 13:37:42 +0800

    单例模式完善

--- 单例模式/index1.php
+++ 单例模式/index1.php
@@ -5,36 +5,59 @@
  *
  * 常见使用场景
  * 1：数据库及资源连接
- * 2：只需获得一个对象实例
+ * 2：日志记录器
+ * 3：只需获得一个对象实例
  */
+
+declare(strict_types=1);
 
 Class A
 {
+    public $num = 0;
 
 }
 
 $a1 = new A();
 $a2 = new A();
+// 浅拷贝
+$a2Clone = clone $a2;
+$a2Copy  = $a2;
 
 // 第二个是返回false的
 var_dump($a1 == $a2);
 var_dump($a1 === $a2);
+$a2->num = 2;
+echo "\$a2 num：" . $a2->num . PHP_EOL;
+var_dump($a2Clone === $a2);
+echo "\$a2Clone num：" . $a2Clone->num . PHP_EOL;
+
+var_dump($a2Copy === $a2);
+echo "\$aCopy num：" . $a2Copy->num . PHP_EOL;
 
 class Singleton
 {
     private static $instance = null;
 
     // 防止外部new
-    private function __construct(){}
+    private function __construct()
+    {
+    }
 
     public static function getInstance()
     {
-        if (static::$instance === null)
-        {
+        if (static::$instance === null) {
             static::$instance = new Singleton();
         }
         return static::$instance;
     }
+
+    /**
+     * prevent the instance from being cloned (which would create a second instance of it)
+     */
+    private function __clone()
+    {
+    }
+
 }
 
 try {
@@ -49,3 +72,6 @@ $b1 = Singleton::getInstance();
 $b2 = Singleton::getInstance();
 // 返回true
 var_dump($b1 === $b2);
+
+// 这里将不被允许
+//$b2Clone = clone $b2;
