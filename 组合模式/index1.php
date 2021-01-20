2f0fb85220ab13383ba342d10cc427261700b48b
xiaoxu <657978106@qq.com>
2021-01-20 15:44:19 +0800

    组合

--- 组合模式/index1.php
+++ 组合模式/index1.php
@@ -8,8 +8,9 @@
 
 abstract class Role
 {
-    protected $userRoleList;
+    protected $userRoleList = [];
     protected $name;
+
     public function __construct(String $name)
     {
         $this->name = $name;
@@ -42,9 +43,57 @@ class RoleManger extends Role
 
     public function SendMessage()
     {
-        echo "开始发送用户角色：" . $this->name . '下的所有用户短信', PHP_EOL;
+        echo "开始发送用户角色：" . $this->name . ' 下的所有用户短信', PHP_EOL;
         foreach ($this->userRoleList as $role) {
             $role->SendMessage();
         }
     }
 }
+
+
+class Team extends Role
+{
+
+    public function Add(Role $role)
+    {
+        echo "小组用户不能添加下级了！", PHP_EOL;
+    }
+
+    public function Remove(Role $role)
+    {
+        echo "小组用户没有下级可以删除！", PHP_EOL;
+    }
+
+    public function SendMessage()
+    {
+        echo "小组用户角色：" . $this->name . ' 的短信已发送！', PHP_EOL;
+    }
+}
+
+
+// root用户
+$root = new RoleManger('root');
+$root->add(new Team('主站用户'));
+$root->SendMessage();
+
+echo "----------------------------------" . PHP_EOL;
+
+// 社交版块
+$root2    = new RoleManger('root-社交');
+$managerA = new RoleManger('论坛用户');
+$managerA->add(new Team('北京论坛用户'));
+$managerA->add(new Team('上海论坛用户'));
+
+
+$managerB = new RoleManger('sns用户');
+$managerB->add(new Team('北京sns用户'));
+$managerB->add(new Team('上海sns用户'));
+
+$root2->add($managerA);
+$root2->add($managerB);
+$root2->SendMessage();
+
+
+
+
+
